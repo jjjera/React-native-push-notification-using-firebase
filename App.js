f8f5c8ccac8ebbe447579bf37b2161c512e79f41
Jerald George <jjjera>
2019-03-04 18:15:39 +0530

    wip

--- App.js
+++ App.js
@@ -1,15 +1,96 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { AsyncStorage } from 'react-native';
+import firebase from 'react-native-firebase';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+export default class App extends Component {
+
+  async componentDidMount() {
+  this.checkPermission();
+  this.createNotificationListeners();
+  }
+
+  componentWillUnmount() {
+  this.notificationListener();
+  this.notificationOpenedListener();
+  }
+
+async createNotificationListeners() {
+/*
+* Triggered when a particular notification has been received in foreground
+* */
+this.notificationListener = firebase.notifications().onNotification((notification) => {
+    const { title, body } = notification;
+    this.showAlert(title, body);
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+/*
+* If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+* */
+this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+    const { title, body } = notificationOpen.notification;
+    this.showAlert(title, body);
+});
+
+/*
+* If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+* */
+const notificationOpen = await firebase.notifications().getInitialNotification();
+if (notificationOpen) {
+    const { title, body } = notificationOpen.notification;
+    this.showAlert(title, body);
+}
+/*
+* Triggered for data only payload in foreground
+* */
+this.messageListener = firebase.messaging().onMessage((message) => {
+  //process data message
+  console.log(JSON.stringify(message));
+});
+}
+
+showAlert(title, body) {
+Alert.alert(
+  title, body,
+  [
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+  ],
+  { cancelable: false },
+);
+}
+
+
+  async checkPermission() {
+  const enabled = await firebase.messaging().hasPermission();
+  if (enabled) {
+      this.getToken();
+  } else {
+      this.requestPermission();
+  }
+}
+
+async getToken() {
+let fcmToken = await AsyncStorage.getItem('fcmToken');
+if (!fcmToken) {
+    fcmToken = await firebase.messaging().getToken();
+    if (fcmToken) {
+        // user has a device token
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+    }
+}
+}
+
+async requestPermission() {
+try {
+    await firebase.messaging().requestPermission();
+    // User has authorised
+    this.getToken();
+} catch (error) {
+    // User has rejected permissions
+    console.log('permission rejected');
+}
+}
+
   render() {
     return (
       <View style={styles.container}>
@@ -25,15 +106,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
